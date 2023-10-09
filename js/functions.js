33ce2eb001094888ba99e8ed31a12c073bd7205b
AAV1ASOV <avworkemails@gmail.com>
2023-10-10 02:36:43 +0500

    after linting

--- js/functions.js
+++ js/functions.js
@@ -3,6 +3,14 @@ function checkStringLength(string, maxLength) {
   return string.length <= maxLength;
 }
 
+// Cтрока короче 20 символов
+checkStringLength('проверяемая строка', 20); // true
+// Длина строки ровно 18 символов
+checkStringLength('проверяемая строка', 18); // true
+// Строка длиннее 10 символов
+checkStringLength('проверяемая строка', 10); // false
+
+
 //Функция для проверки, является ли строка палиндромом
 function isPalindrome(string) {
   string = string.replaceAll(' ', '').toLowerCase();
@@ -15,6 +23,16 @@ function isPalindrome(string) {
   return string === reverseString;
 }
 
+// Строка является палиндромом
+isPalindrome('топот'); // true
+// Несмотря на разный регистр, тоже палиндром
+isPalindrome('ДовОд'); // true
+// Это не палиндром
+isPalindrome('Кекс'); // false
+// Это палиндром
+isPalindrome('Лёша на полке клопа нашёл '); // true
+
+
 //Функция полученияя целого положительного числа из строки
 function getPositiveNumber(string) {
   string = string.toString();
@@ -23,9 +41,19 @@ function getPositiveNumber(string) {
   for (let i = 0; i < string.length; i++) {
 
     for (let j = 0; j <= 9; j++) {
-      if (parseInt(string[i]) !== j) continue;
+      if (parseInt(string[i], 10) !== j) {
+        continue;
+      }
       number += string[i];
     }
   }
-  return parseInt(number);
+  return parseInt(number, 10);
 }
+
+getPositiveNumber('2023 год'); // 2023
+getPositiveNumber('ECMAScript 2022'); // 2022
+getPositiveNumber('1 кефир, 0.5 батона'); // 105
+getPositiveNumber('а я томат'); // NaN
+getPositiveNumber(2023); // 2023
+getPositiveNumber(-1); // 1
+getPositiveNumber(1.5); // 15
