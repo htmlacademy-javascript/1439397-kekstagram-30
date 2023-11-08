d027bc64ebd53592139ed91cbdbff39def580e1b
AAV1ASOV <avworkemails@gmail.com>
2023-11-09 03:11:23 +0500

    module8-task1 alpha 0.1

--- js/big-picture.js
+++ js/big-picture.js
@@ -1,6 +1,7 @@
 import './thumbnails.js';
 import { container } from './thumbnails.js';
 import { photoDescriptionCollection } from './photo-description.js';
+import { isEscapeKey } from './utils.js'
 
 const bigPictureModalElement = document.querySelector('.big-picture');
 const bigPictureElement = bigPictureModalElement.querySelector('.big-picture__img img');
@@ -10,57 +11,80 @@ const commentTotalElement = bigPictureModalElement.querySelector('.social__comme
 const socialComments = document.querySelector('.social__comments');
 const socialComment = document.querySelector('.social__comment');
 const socialCaption = document.querySelector('.social__caption');
-
 const cancelButton = document.querySelector('.big-picture__cancel');
+const commentCount = bigPictureModalElement.querySelector('.social__comment-count');
+const commentsLoader = bigPictureModalElement.querySelector('.comments-loader');
 
 const pictures = photoDescriptionCollection();
-console.log(pictures)
 
-const createComment = (pictures) => {
+const renderBigPictureData = (evt) => {
+  bigPictureElement.src = evt.target.src;
+  likesCountElement.textContent = evt.target.nextElementSibling.lastElementChild.textContent;
+  commentShownElement.textContent = 5;
+  commentTotalElement.textContent = evt.target.nextElementSibling.firstElementChild.textContent;
+  socialCaption.textContent = evt.target.alt;
+
+  const thumbnailIndex = parseInt((evt.target.src.split('').splice(29, 2).join('')), 10);
+  const thumbnailCommentsArray = pictures[thumbnailIndex - 1].comments;
+  socialComments.innerHTML = '';
+  renderComment(thumbnailCommentsArray);
+}
+
+const createComment = ({ avatar, name, message }) => {
   const comment = socialComment.cloneNode(true);
 
-  comment.querySelector('.social__picture').src = pictures.avatar;
-  comment.querySelector('.social__picture').alt = pictures.name;
-  comment.querySelector('.social__text').textContent = pictures.message;
+  comment.querySelector('.social__picture').src = avatar;
+  comment.querySelector('.social__picture').alt = name;
+  comment.querySelector('.social__text').textContent = message;
 
   return comment;
 }
 
-const renderComment = (obj) => {
+const renderComment = (commentsArray) => {
   const commentFragment = document.createDocumentFragment();
 
-  obj.forEach((obj) => {
-    const comment = createComment(obj);
+  commentsArray.forEach((commentItem) => {
+    const comment = createComment(commentItem);
     commentFragment.append(comment);
   });
 
   socialComments.append(commentFragment);
 };
 
+const onDocumentKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closeBigPicture();
+  }
+};
+
+const openBigPicture = () => {
+  bigPictureModalElement.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+
+  commentCount.classList.add('hidden');
+  commentsLoader.classList.add('hidden');
+
+  container.addEventListener('keydown', onDocumentKeydown)
+}
+
+const closeBigPicture = () => {
+  bigPictureModalElement.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+
+  container.removeEventListener('keydown', onDocumentKeydown)
+}
+
 container.addEventListener('click', (evt) => {
   if (evt.target.className === 'picture__img') {
-    bigPictureModalElement.classList.remove('hidden');
-    bigPictureElement.src = evt.target.src;
-    likesCountElement.textContent = evt.target.nextElementSibling.lastElementChild.textContent;
-    commentShownElement.textContent = 5;
-    commentTotalElement.textContent = evt.target.nextElementSibling.firstElementChild.textContent;
-    socialCaption.textContent = evt.target.alt;
-
-    const index = evt.target.src.split('').slice(29).find(item => parseInt(item, 10));
-    console.log(index);
-    const obj = pictures[index - 1].comments;
-    console.log(obj)
-    socialComments.innerHTML = '';
-    renderComment(obj);
+    openBigPicture();
+    renderBigPictureData(evt);
   }
-
 });
 
 cancelButton.addEventListener('click', () => {
-  bigPictureModalElement.classList.add('hidden');
-})
-
-
+  closeBigPicture();
+});
 
 export { pictures }
 
